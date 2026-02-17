67238c8e2944463e1c9ec05e5c3bdaf90244c4a8
Md Fuzail <saiffardin1@gmail.com>
2026-02-17 14:15:32 -0800

    updated add-Shipments Controller

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllCustomers } = require('../controllers/customerController');
-const { authMiddleware } = require('../middleware/authMiddleware');
+const getAllCustomers = require('../controllers/customerController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Get All Customers Route
-router.get('/all', authMiddleware, getAllCustomers);
+router.get('/all', authMiddleware, getAllCustomers.createCustomerFromShipment);
 
 
 module.exports = router;
