33a9029b5ba172099a18d6b396cb5cc5dbd77b56
Md Fuzail <saiffardin1@gmail.com>
2026-02-18 11:49:13 -0800

    updated wallet Add Money

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getWalletBalance, addFunds } = require("../controllers/walletController");
+const { getWalletBalance, addFunds, getWalletTransactions } = require("../controllers/walletController");
 const customerAuth = require("../middleware/customerAuth");
+
 
 // Get Wallet Balance Route
 router.get("/balance", customerAuth, getWalletBalance);
 
+
+// Get Wallet Transactions Route
+router.get("/transactions", customerAuth, getWalletTransactions);
+
 // Add Funds to Wallet Route
-// router.post("/add-funds", customerAuth, addFunds);
+router.post("/add-funds", customerAuth, addFunds);
+
 
 module.exports = router;
