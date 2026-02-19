d9d9d14a051ed1f4eb9c22074f8b58702c8d1823
Md Fuzail <saiffardin1@gmail.com>
2026-02-19 12:30:17 -0800

    Add & Update Support Service

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 exports.sendTicketMail = async (to, subject, htmlContent) => {
     try {
     await resend.emails.send({
-        from: process.env.EMAIL_USER,
+        from: process.env.EMAIL_FROM,
         to,
         subject,
         html: htmlContent
