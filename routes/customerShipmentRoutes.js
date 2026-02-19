06f5b3b5876b52a3d00e390535e5e8156fa6fb4a
Md Fuzail <saiffardin1@gmail.com>
2026-02-19 02:03:44 -0800

    Add & Update Profile Section Created

--- routes/customerShipmentRoutes.js
+++ routes/customerShipmentRoutes.js
@@ -5,8 +5,11 @@ const { customerAuth } = require("../middleware/customerAuth");
 const { shipmentController } = require("../controllers/customerShipmentController");
 
 console.log("customer shipment controller:", shipmentController);
+console.log("type of shipmentController:", typeof shipmentController);
 console.log("getCustomerShipments function:", shipmentController.getCustomerShipments);
 console.log("Type of getCustomerShipments:", typeof shipmentController.getCustomerShipments);
+console.log("customerAuth middleware:", customerAuth);
+console.log("Type of customerAuth middleware:", typeof customerAuth);
 
 
 // Get Customer Shipments Route
