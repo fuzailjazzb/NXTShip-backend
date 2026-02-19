badf8e77d2145aa711c4aa148bb9bfbaf1036289
Md Fuzail <saiffardin1@gmail.com>
2026-02-19 01:47:10 -0800

    Add & Update Profile Section Created

--- controllers/customerShipmentController.js
+++ controllers/customerShipmentController.js
@@ -2,10 +2,25 @@ const shipment = require("../models/shipment");
 
 exports.getCustomerShipments = async (req, res) => {
   try {
+
+    console.log("Get Shipments API hit for customer:", req.customer._id);
+
     const shipments = await shipment.find({ customerId: req.customer._id }).sort({ createdAt: -1 });
 
-    res.json({ shipments });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    console.log(`Found ${shipments.length} shipments for customer ${req.customer._id}`);
+
+      res.status(200).json({
+        success: true,
+        total: shipments.length,
+        shipments,
+      }); 
+  } catch (error) {
+    console.log("Error fetching shipments for customer:", error.message);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  } 
 };
