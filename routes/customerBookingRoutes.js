1d2627b69e240bcb274a2d8f676a03f7509f2ced
Md Fuzail <saiffardin1@gmail.com>
2026-02-27 09:19:56 -0800

    Add & Update Resources Section Created

--- routes/customerBookingRoutes.js
+++ routes/customerBookingRoutes.js
@@ -1,12 +1,8 @@
-
-
 const express = require("express");
 const router = express.Router();
 
 const { bookCustomerShipment } = require("../controllers/customerBookingController");
 const customerAuth = require("../middleware/customerAuth");
-
-console.log(typeof bookCustomerShipment);
 
 // 🔐 Customer Create Shipment
 router.post("/create", customerAuth, bookCustomerShipment);
