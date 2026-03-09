59c23979824c1d0922a4cb06ba2eca1774ae0c01
Md Fuzail <saiffardin1@gmail.com>
2026-03-09 09:40:15 -0700

    Add & Update Add Product Details

--- routes/customerBookingRoutes.js
+++ routes/customerBookingRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { bookCustomerShipment } = require("../controllers/customerBookingController");
+const courierEngineController = require("../controllers/courierEngineController");
 const { customerAuth } = require("../middleware/customerAuth");
 
 console.log(typeof bookCustomerShipment);
@@ -11,7 +11,7 @@ router.post("/book",
     (req, res, next) => {
         console.log("Shipment Route Hit.....");
         next();
-    },customerAuth, bookCustomerShipment);
+    },customerAuth, courierEngineController.bookShipment);
 
 
 
