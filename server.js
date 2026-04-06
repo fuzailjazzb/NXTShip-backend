a65e352d9c9b84029a9c010d56b334b0adfbf776
Md Fuzail <saiffardin1@gmail.com>
2026-04-06 19:00:00 +0530

    API INTEGRATIONS

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.use("/api/integrations", require("./routes/integrationRoutes"));
 
 // warehouse allotment
 app.use("/api/warehouse-allotment", warehouseAllotmentRoutes);
-app.use("/api/warehouse-allotment", require("./routes/warehouseAllotmentRoutes"));
+
 
 // Webhook
 app.use("/webhook", require("./routes/webhookRoutes"));
